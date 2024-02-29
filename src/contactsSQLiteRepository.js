aa53aa6a515b2d8c344da83c30fc654459405acb
Naveena Kandasamy Rajkumar <naveraj07@gmail.com>
2024-02-29 14:47:49 -0600

    lab 3 - changes in creating, getting and deleteing contacts from sqlite

--- src/contactsSQLiteRepository.js
+++ src/contactsSQLiteRepository.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
+const Contact = require('./Contact');
 
 const betterSqlite3 = require('better-sqlite3');
 const db = new betterSqlite3(path.join(__dirname, '../data/contactDetails.sqlite'), { verbose: console.log });
 
 //db.set('405226f4-0a2c-4ae0-b857-aa18d45cb12f',{id: '405226f4-0a2c-4ae0-b857-aa18d45cb12f', firstName: 'Michelle', lastName: 'Scott', email: 'mitchelle@gmail.com'});
-const createStatement = db.prepare("CREATE TABLE IF NOT EXISTS contactdb (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastName TEXT, email TEXT)");
+const createStatement = db.prepare("CREATE TABLE IF NOT EXISTS contactdb(id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastName TEXT, email TEXT, notes TEXT, lastupdatedate TEXT)");
 createStatement.run();
 
 /*const loadDataFromFile = () => {
@@ -28,20 +29,29 @@ const saveDataInFile = () => {
 
 const repo = {
     findAll: () => {
-        //return Array.from(db.values());
-        return [];
+        const st = db.prepare("SELECT * FROM contactdb");
+        const rows = st.all();
+        let contacts = [];
+        rows.forEach((row)=>{
+            const contact = new Contact(row.id, row.firstname, row.lastName, row.email, row.notes);
+            contacts.push(contact);
+        })
+        return contacts;
     },
     findContactById: (id) => {
-        //db.get(id);
-        return {};
+        const st = db.prepare("SELECT * from contactdb WHERE id = ?");
+        const row = st.get(id);
+        return new Contact(row.id, row.firstname, row.lastName, row.email, row.notes, row.lastupdatedate);
     },
     deleteContactById: (id) => { 
-        // db.delete(id);
-        // saveDataInFile();
+        const st = db.prepare("DELETE FROM contactdb WHERE id = ?");
+        const row = st.run(id);
+        console.log(`Rows affected: ${row.changes}`);
     },
     addNewContact: (id,data) => {
-        // db.set(id,data);
-        // saveDataInFile();
+        const st = db.prepare("INSERT INTO contactdb (firstname, lastname, email, notes, lastupdatedate) VALUES (?,?,?,?,?)");
+        const row = st.run(data.firstName, data.lastName, data.email, data.notes, data.lastUpdateDate);
+        console.log(`Contact created with id: ${row.lastInsertRowid}`);
     },
     updateExistingContact: (data) => {
         // db.set(data.id, data);
